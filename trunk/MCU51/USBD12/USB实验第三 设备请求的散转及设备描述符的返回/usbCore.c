b23dd144a9f2a8c67d2c12e834fa62ef850a3540
Dean.Sinaean@gmail.com <Dean.Sinaean@gmail.com@19657033-0acc-cfff-0a07-3c241eebd25e>
2011-04-15 09:16:31 +0000

    设备请求散转,OK

--- trunk/MCU51/USBD12/USB实验第三 设备请求的散转及设备描述符的返回/usbCore.c
+++ trunk/MCU51/USBD12/USB实验第三 设备请求的散转及设备描述符的返回/usbCore.c
@@ -6,6 +6,15 @@
 
 idata u8 Buffer[16];
 
+u8 bmRequestType;
+u8 bRequest;
+u16 wValue;
+u16 wIndex;
+u16 wLength;
+u8 * pSendData;
+u16 wSendLength;
+u8 NeedZeroPacket;
+
 void UsbBusSuspend(void)
 {}
 void UsbBusReset(void)
@@ -20,7 +29,155 @@ void UsbEp0Out(void)
 		D12ReadEndPointBuffer(0,16,Buffer);
 		D12AckSetup();
 		D12ClearBuffer();
-	}
+
+		bmRequestType=Buffer[0];
+		bRequest=Buffer[1];
+		wValue=Buffer[2]+(((u16)Buffer[3])<<8);
+		wIndex=Buffer[4]+(((u16)Buffer[5])<<8);
+		wLength=Buffer[6]+(((u16)Buffer[7])<<8);
+
+		if((bmRequestType&0x80)==0x80)
+		{
+			switch((bmRequestType>>5)&0x03)
+			{
+				case 0:
+				#ifdef DEBUG
+				Prints("Standard USB request.\n");
+				#endif
+			   	switch(bRequest)
+				{
+					case GET_CONFIGURATION:
+					#ifdef DEBUG
+					Prints("Get configuration.\n");
+					#endif
+					break;
+	
+					case GET_DESCRIPTOR:
+					#ifdef DEBUG
+					Prints("Get descriptor.\n");
+					#endif
+					break;
+	
+					case GET_INTERFACE:
+					#ifdef DEBUG
+					Prints("Get Interface.\n");
+					#endif
+					break;
+	
+					case GET_STATUS:
+					#ifdef DEBUG
+					Prints("Get status.\n");
+					#endif
+					break;
+	
+					case SYNCH_FRAME:
+					#ifdef DEBUG
+					Prints("Synch frame.\n");
+					#endif
+					break;
+	
+					default:
+					#ifdef DEBUG
+					Prints("Error,undefined standard device request.\n");
+					#endif
+					break;
+				}
+				break;
+	
+				case 1:   //Class request.
+				#ifdef DEBUG
+				Prints("Class request.\n");
+				#endif
+				break;
+	
+				case 2: //³§ÉÌÇëÇó
+				#ifdef DEBUG
+				Prints("Vendor request.\n");
+				#endif
+				break;
+	
+				default:
+				#ifdef DEBUG
+				Prints("Error,undefined request.\n");
+				#endif
+				break;
+			}
+		}
+		else
+		{
+			switch((bmRequestType>>5)&0x03)
+			{
+				case 0:
+				#ifdef DEBUG
+				Prints("Standard out.\n");
+				#endif
+				break;
+	
+				switch(bRequest)
+				{
+					case CLEAR_FEATURE:
+					#ifdef DEBUG
+					Prints("Clear feature.\n");
+					#endif
+					break;
+	
+					case SET_ADDRESS:
+					#ifdef DEBUG
+					Prints("Set address.\n");
+					#endif
+					break;
+	
+					case SET_CONFIGURATION:
+					#ifdef DEBUG
+					Prints("Set configuration.\n");
+					#endif
+					break;
+	
+					case SET_DESCRIPTOR:
+					#ifdef DEBUG
+					Prints("Set descriptor.\n");
+					#endif
+					break;
+	
+					case SET_FEATURE:
+					#ifdef DEBUG
+					Prints("Set feature.\n");
+					#endif
+					break;
+	
+					case SET_INTERFACE:
+					#ifdef DEBUG
+					Prints("Set interface.\n");
+					#endif
+					break;
+	
+					default:
+					#ifdef DEBUG
+					Prints("Error,undefined output request.\n");
+					#endif
+					break;
+				}
+	
+				case 1:  //class request.
+				#ifdef DEBUG
+				Prints("Class output request.");
+				#endif
+				break;
+	
+				case 2: //vendor request.
+				#ifdef DEBUG 
+				Prints("Vendor output request.\n");
+				#endif
+				break;
+	
+				default:
+				#ifdef DEBUG
+				Prints("Error,undefined output request.\n");
+				#endif
+				break;
+			}
+		}
+ 	}
 	else
 	{
 		D12ReadEndPointBuffer(0,16,Buffer);
--- trunk/MCU51/USBD12/USB实验第三 设备请求的散转及设备描述符的返回/usbCore.h
+++ trunk/MCU51/USBD12/USB实验第三 设备请求的散转及设备描述符的返回/usbCore.h
@@ -5,9 +5,22 @@
 # define __USBCORE_H__
 
 #include "MCU51_Types.h"
+#include "config.h"
 #include <at89x52.h>
 #include "uart.h"
 #include "d12.h"
+
+#define GET_STATUS         0
+#define CLEAR_FEATURE      1
+#define SET_FEATURE        3
+#define SET_ADDRESS        5
+#define GET_DESCRIPTOR     6
+#define SET_DESCRIPTOR     7
+#define GET_CONFIGURATION  8
+#define SET_CONFIGURATION  9
+#define GET_INTERFACE      10
+#define SET_INTERFACE      11
+#define SYNCH_FRAME        12
 
 extern idata u8 Buffer[16];
 void UsbBusSuspend(void);
